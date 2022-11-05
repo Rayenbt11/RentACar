57eef1084a06b52c0ec9b1ad815ae019d2ac1801
Rayenbt11 <2021378@student.cct.ie>
2022-11-05 14:24:49 +0000

    update booking system

--- src/ooc/yoursolution/BookingSystem.java
+++ src/ooc/yoursolution/BookingSystem.java
@@ -6,16 +6,36 @@ package ooc.yoursolution;
 
 import java.io.BufferedReader;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import ooc.enums.Make;
 
 /**
  *
  * @author GAMERS
  */
-public class BookingSystem implements BookingSystemInterface{
+public class BookingSystem implements BookingSystemInterface {
 
     @Override
     public RentACarInterface setupRentACar(BufferedReader in) throws IOException {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+        String name = in.readLine();
+        List<CarInterface> cars = new ArrayList<>();
+        while (in.readLine() != null) {
+            String[] splitter = in.readLine().split(":");
+            Make make = Make.valueOf(splitter[1]);
+            double rate = Double.parseDouble(splitter[2]);
+            int numberOfCars = Integer.parseInt(splitter[3]);
+            for (int i = 0; i < numberOfCars; i++) {
+                int id = 0;
+                Car newCar = new Car(id, make, rate);
+                cars.add(newCar);
+
+            }
+
+        }
+        RentACarInterface carRental = new RentACar(name, cars);
+
+        return carRental;
     }
-    
+
 }
