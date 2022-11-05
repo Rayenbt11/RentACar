163fff1a28b423149e80800261187fc143a4f64c
Rayenbt11 <2021378@student.cct.ie>
2022-11-05 16:33:05 +0000

    BookingSystem class done

--- src/ooc/yoursolution/BookingSystem.java
+++ src/ooc/yoursolution/BookingSystem.java
@@ -16,26 +16,37 @@ import ooc.enums.Make;
  */
 public class BookingSystem implements BookingSystemInterface {
 
+    int id = 0;
+
     @Override
     public RentACarInterface setupRentACar(BufferedReader in) throws IOException {
-        String name = in.readLine();
-        List<CarInterface> cars = new ArrayList<>();
+        String name = in.readLine(); // the title will be the first line
+        List<CarInterface> cars = new ArrayList<>(); // list to contain cars
         while (in.readLine() != null) {
+            /* different data inside the text file so we will split it by the ":"
+            it works the same as the delimiter in the scanner
+             */
             String[] splitter = in.readLine().split(":");
-            Make make = Make.valueOf(splitter[1]);
-            double rate = Double.parseDouble(splitter[2]);
-            int numberOfCars = Integer.parseInt(splitter[3]);
+            Make make = Make.valueOf(splitter[1]); // first part will be the make
+            double rate = Double.parseDouble(splitter[2]); // second part will be the rate 
+            int numberOfCars = Integer.parseInt(splitter[3]); // third part will be number of cars 
+
             for (int i = 0; i < numberOfCars; i++) {
-                int id = 0;
+                /*we will have at least one car of that make but we can have more than one 
+                so each car should be added, that's why I used a for loop that will be used once at least for each
+                line.
+                each car will have it's own id as well
+                 */
+
                 Car newCar = new Car(id, make, rate);
                 cars.add(newCar);
-
+                id++;
             }
 
         }
-        RentACarInterface carRental = new RentACar(name, cars);
+        RentACarInterface rentACar = new RentACar(name, cars);
 
-        return carRental;
+        return rentACar;
     }
 
 }
